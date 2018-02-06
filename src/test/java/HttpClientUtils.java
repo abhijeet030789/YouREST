0f6fa1df86d8914aa2e6e5c9926261ccb3dc2c2b
dwiveddi <divyashree.dwivedi@gmail.com>
2018-02-06 20:24:59 +0530

    HttpClient

---
+++ src/test/java/HttpClientUtils.java
@@ -0,0 +1,55 @@
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.http.HttpRequest;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.*;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by dwiveddi on 2/6/2018.
+ */
+public class HttpClientUtils {
+
+    public static HttpClient getHttpClient() {
+        RequestConfig globalConfig =  RequestConfig.custom()
+                        .setConnectTimeout(1000)
+                        .setConnectionRequestTimeout(1000)
+                        .setSocketTimeout(5000)
+                        .setCookieSpec(CookieSpecs.DEFAULT)
+                        .setRedirectsEnabled(false)
+                        .build();
+
+        HttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(globalConfig)
+                .setMaxConnTotal(500)
+                .setMaxConnPerRoute(100)
+                .setConnectionTimeToLive(600, TimeUnit.SECONDS)
+                .evictIdleConnections(600, TimeUnit.SECONDS)
+                .build();
+        return httpClient;
+    }
+
+    public static HttpRequestBase getHTTPBase(String path, String httpMethod, String payload) throws UnsupportedEncodingException {
+        HttpRequestBase httpRequestBase = null;
+        switch (httpMethod){
+            case "GET" :  httpRequestBase =  new HttpGet(path); break;
+            case "POST": httpRequestBase = new HttpPost(path); break;
+            case "PUT": httpRequestBase =  new HttpPut(path); break;
+            case "PATCH": httpRequestBase = new HttpPatch(path); break;
+            case "DELETE" : httpRequestBase =  new HttpDelete(path); break;
+            default: throw new IllegalArgumentException("Unhandled httpMethod " + httpMethod);
+        }
+        if(httpRequestBase instanceof HttpEntityEnclosingRequestBase){
+            HttpEntityEnclosingRequestBase httpEntityEnclosingRequestBase = (HttpEntityEnclosingRequestBase) httpRequestBase;
+            httpEntityEnclosingRequestBase.setEntity(new StringEntity(payload));
+        }
+        return httpRequestBase;
+    }
+
+
+}
--- src/test/java/PremiumTweetsServiceTest.java
+++ src/test/java/PremiumTweetsServiceTest.java
@@ -1,29 +1,35 @@
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.HttpClients;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+
 
 @RunWith(Parameterized.class)
 public class PremiumTweetsServiceTest {
 
+    private static HttpClient httpClient = HttpClientUtils.getHttpClient();
+
+    @BeforeClass
+    public static void setup(){
+
+    }
+
     private Api api;
-    private static boolean isReuiredToPass = true;
 
     @Parameterized.Parameters
-    public static Collection data() throws IOException {
+    public static Api[] data() throws IOException {
         File file =  new File("D:/OpenSource/rest-api-test/src/test/resources/rest-api-contracts.json");
-        Api[] apiArr
-                = new ObjectMapper().readValue(file, Api[].class);
-
-        return Arrays.asList(apiArr);
-                /*{99, 9.90}, {100, 10.00}, {101, 10.08}, {200, 18},
-                {499, 41.92}, {500, 42}, {501, 42.05}, {1000, 67},
-                {10000, 517},});*/
+        return new ObjectMapper().readValue(file, Api[].class);
     }
 
     public PremiumTweetsServiceTest(Api api) {
@@ -33,7 +39,9 @@ public class PremiumTweetsServiceTest {
 
     @Test
     public void shouldCalculateCorrectFee() {
+        HttpRequestBase httpRequestBase = HttpClientUtils.getHttpClient(api.getUrl(), api.getMethod(), api.getRequestResponseCombinations().get(0).getRequest().getPayload());
         System.out.println(String.format("api = %s", api));
-        isReuiredToPass = false;
     }
+
+
 }
